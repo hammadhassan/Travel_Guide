94601fa8714bd8a2235c816f59eeb80dbf1f9463
hammad Hassan <hammadhassansheikh@gmail.com>
2016-09-23 15:07:54 +0500

    apply numbers

--- app.js
+++ app.js
@@ -15,3 +15,8 @@ alert(name);
 var name = prompt("Please enter first name");
 var name1 = prompt("Please enter last name");
 alert(name+" "+name1);
+
+//
+var num = prompt("Enter any number");
+var Num = prompt("Enter any number");
+alert(num / Num);
