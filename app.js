40fda6260c816102c4e3d2f3d1b5a7198bc0bba3
hammad Hassan <hammadhassansheikh@gmail.com>
2016-09-23 15:01:33 +0500

    apply prompt

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-alert("welcome to Java script");
+/*alert("welcome to Java script");
 alert("Welcome to JS Land... \n Happy Coding!");
 var a = undefined;
 alert(a);
 var b = undefined;
 alert(b);
 alert(a + b);
+*/
+
+//Enter Your Name
+var name = prompt("Please enter your name");
+alert(name);
+
+//Enter Your First and Last Name
+var name = prompt("Please enter first name");
+var name1 = prompt("Please enter last name");
+alert(name+" "+name1);
